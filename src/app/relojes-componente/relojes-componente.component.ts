43d2b194b936d6cf972a46941286ee0d58eb5d7f
Yeray Blanco Aldao <yerayblanco@hotmail.com>
2025-02-13 17:54:12 +0100

    Maquetacion Pagina Zapatillas

--- src/app/relojes-componente/relojes-componente.component.ts
+++ src/app/relojes-componente/relojes-componente.component.ts
@@ -13,31 +13,37 @@ export class RelojesComponenteComponent {
  relojes = [
     {
       marca: 'Garmin Fenix 7',
+      alt:"Garmin Fenix 7",
       imagen: 'https://m.media-amazon.com/images/I/81ZQJ93uReL._AC_UF894,1000_QL80_.jpg',
       puntuacion: 4.8
     },
     {
       marca: 'Polar Vantage V2',
+      alt:"Polar Vantage V2",
       imagen: 'https://m.media-amazon.com/images/I/71FfH-+Me1L._AC_UF894,1000_QL80_.jpg',
       puntuacion: 4.6
     },
     {
       marca: 'Suunto 9 Peak',
+      alt:"Suunto 9 Peak",
       imagen: 'https://m.media-amazon.com/images/I/71skBW-WnxL._AC_UF894,1000_QL80_.jpg',
       puntuacion: 4.5
     },
     {
       marca: 'Apple Watch Ultra',
+      alt:"Apple Watch Ultra",
       imagen: 'https://m.media-amazon.com/images/I/81T9x6oD4aL._AC_UF894,1000_QL80_.jpg',
       puntuacion: 4.9
     },
     {
       marca: 'Apple Watch SE',
+      alt:"Apple Watch SE",
       imagen: 'https://m.media-amazon.com/images/I/81T9x6oD4aL._AC_UF894,1000_QL80_.jpg',
       puntuacion: 4.9
     },
     {
       marca: 'Garmin Forerunner 55',
+      alt:"Garmin Forerunner 55",
       imagen: 'https://m.media-amazon.com/images/I/81T9x6oD4aL._AC_UF894,1000_QL80_.jpg',
       puntuacion: 4.9
     },
--- src/app/zapatillas-componente/zapatillas-componente.component.ts
+++ src/app/zapatillas-componente/zapatillas-componente.component.ts
@@ -1,11 +1,72 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-zapatillas-componente',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './zapatillas-componente.component.html',
   styleUrl: './zapatillas-componente.component.css'
 })
 export class ZapatillasComponenteComponent {
+
+  zapatillas=[
+
+    {
+      marca: 'Nike',
+      imagen: 'https://via.placeholder.com/150',
+      alt: 'Imagen de zapatillas Nike',
+      modelo: 'Pegasus 37',
+      precio: 120,
+      puntuacion: 4.5
+    },
+    {
+      marca: 'Addidas',
+      imagen: 'https://via.placeholder.com/150',
+      alt: 'Imagen de zapatillas Nike',
+      modelo: 'Airmax',
+      precio: 120,
+      puntuacion: 4.5
+    },
+    {
+      marca: 'Puma',
+      imagen: 'https://via.placeholder.com/150',
+      alt: 'Imagen de zapatillas Nike',
+      modelo: 'Airmax',
+      precio: 120,
+      puntuacion: 4.5
+    },
+    {
+      marca: 'Skechers',
+      imagen: 'https://via.placeholder.com/150',
+      alt: 'Imagen de zapatillas Nike',
+      modelo: 'Airmax',
+      precio: 120,
+      puntuacion: 4.5
+    },
+    {
+      marca: 'Asics',
+      imagen: 'https://via.placeholder.com/150',
+      alt: 'Imagen de zapatillas Nike',
+      modelo: 'Airmax',
+      precio: 120,
+      puntuacion: 4.5
+    },
+    {
+      marca: 'Reebok',
+      imagen: 'https://via.placeholder.com/150',
+      alt: 'Imagen de zapatillas Nike',
+      modelo: 'Airmax',
+      precio: 120,
+      puntuacion: 4.5
+    }
+   
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
 
 }
